b1dd98823f2bc22bb02fc9602092463c90b88be2
Jeremie Bonal <jeremie.bonal@gmail.com>
2016-09-07 11:15:34 +0200

    Changed the frequency of the sensor retrieval.

--- config/initializers/cap_trap_initializer.rb
+++ config/initializers/cap_trap_initializer.rb
@@ -2,6 +2,6 @@ Cap2020::CapTrapIntegration.on_check_success do
   Cap2020::CapTrapFetchUpdateCreateJob.perform_later
 end
 
-Cap2020::CapTrapIntegration.run every: :hour do
+Cap2020::CapTrapIntegration.run every: :day do
   Cap2020::CapTrapFetchUpdateCreateJob.perform_now
 end
